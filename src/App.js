6748ce4152dc216c56eafca117ac51ba4678b387
jasonagola <jason.agola@gmail.com>
2022-11-30 11:49:09 -0500

    module exclusion

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Queue from "./Queue";
 import {Amplify} from 'aws-amplify';
 import awsconfig from './aws-exports';
-import { Authenticator, ThemeProvider } from '@aws-amplify/ui-react'
+// import { Authenticator, ThemeProvider } from '@aws-amplify/ui-react'
 
 
 Amplify.configure(awsconfig)
@@ -10,13 +10,13 @@ Amplify.configure(awsconfig)
 function App() {
 
     return (
-        <Authenticator>
+        // <Authenticator>
             <div>
                 <h1>Local Bike Shop NFK</h1>
                 <h3>Service Manager</h3>
                 <Queue></Queue>
             </div>
-        </Authenticator>
+        // </Authenticator>
     )
 }
 
